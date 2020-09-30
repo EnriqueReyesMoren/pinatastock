10e0ffc91670d506ac63359640173fedb3b75a09
EnriqueReyesMoren <enriquereyesmoren@gmail.com>
2020-09-29 22:32:03 -0500

    fist deploy ever

---
+++ public/build/precache-manifest.a21d9ef046f65c7aea395ed579522920.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4c97e52df844c98891dd3a0ef78cce23",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1a73cc912c68793400dc",
+    "url": "/static/css/2.dc10c089.chunk.css"
+  },
+  {
+    "revision": "ee64e55c64b372348924",
+    "url": "/static/css/main.daac1ec2.chunk.css"
+  },
+  {
+    "revision": "1a73cc912c68793400dc",
+    "url": "/static/js/2.6c3da267.chunk.js"
+  },
+  {
+    "revision": "ee64e55c64b372348924",
+    "url": "/static/js/main.dcd6d483.chunk.js"
+  },
+  {
+    "revision": "fb47f3c02db3ce888eb4",
+    "url": "/static/js/runtime-main.dc128e9d.js"
+  }
+]);
